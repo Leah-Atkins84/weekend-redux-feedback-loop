37a3423474aad3fabde3034f6e6bb7537b39a0ba
Leah Atkins <leah.atkins84@gmail.com>
2022-02-27 14:02:49 -0600

    Added link/button/route  to success page/back to home

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-import {useDispatch} from 'react-redux';
+// import {useDispatch} from 'react-redux';
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import FeedBackOne from '../FeedBackOne/FeedBackOne';
 import Understanding from '../Understanding/Understanding';
@@ -10,61 +10,51 @@ import Comments from '../Comments/Comments';
 import ReviewPage from '../ReviewPage/ReviewPage';
 
 function App() {
-    // const dispatch = useDispatch();
-
-    // const getFeedBack = () => {
-    // axios.get('/api/pizza')
-    // .then(response => {
-    //     dispatch({
-    //       type: 'FEED_BACK_ONE',
-    //       payload: response.data
-    //     });
-
-    // }).catch(error => {
-    //     console.log('Error in feedbackone', error);
-    //     alert('NO feedback FOR YOU')
-    // })
-    // }
+  
 
 
-    return (
-        <Router>
-            <div className='App'>
-                <header className='App-header'>
-                    <h1 className='App-title'>Feedback!</h1>
-                    <h4>Don't forget it!</h4>
-                </header>
-                <Route path="/" exact>
-                    <FeedBackOne/>
-                </Route>
+    return (<Router>
+        <div className='App'>
+            <header className='App-header'>
+                <h1 className='App-title'>Feedback!</h1>
+                <h4>Don't forget it!</h4>
+            </header>
 
-                <Route path="/Understanding" exact>
-                    <Understanding/>
-                </Route>
+            <Route path="/" exact>
+                <FeedBackOne/>
+            </Route>
 
-                <Route path="/Supported" exact>
-                    <Supported/>
-                </Route>
+            <Route path="/Understanding" exact>
+                <Understanding/>
+            </Route>
 
-                <Route path="/Comments" exact>
-                    <Comments/>
-                </Route>
+            <Route path="/Supported" exact>
+                <Supported/>
+            </Route>
 
-                <Route path="/ReviewPage" exact>
-                  <ReviewPage/>
-                </Route>
-                <Route path="/Success" exact>
-                  <h1>Success!!!</h1>
-                  
-                </Route>
+            <Route path="/Comments" exact>
+                <Comments/>
+            </Route>
+
+            <Route path="/ReviewPage" exact>
+                <ReviewPage/>
+            </Route>
+
+            <Route path="/Success" exact>
+                <h1>Thankyou for your feedback!!!</h1>
+                <Link to="/"><button> Leave new feedback</button> </Link> 
+                   
+          
+
+            </Route>
 
 
-            </div>
-        </Router>
+        </div>
+    </Router>
 
 
     );
-}
-
-
-export default App;
+    }
+    
+    
+    export default App;
